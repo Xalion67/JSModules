4399d85d6cd300a2f0a0955ad0641277c6aae721
Kostya Parfenov <xalion67@gmail.com>
2017-02-06 11:31:10 +0300

    code for invertion register letter in str

---
+++ invertCase.js
@@ -0,0 +1,11 @@
+import * as strings from './strings';
+
+
+ export const invertCase = str => {
+   let resultStr = '';
+   for (let i = 0; i < strings.length(str); i++) {
+     str[i] === strings.toUpperCase(str[i]) ? resultStr += strings.toLowerCase(str[i]) : resultStr += strings.toUpperCase(str[i]);
+   }
+   console.log(resultStr);
+   return resultStr;
+ };
